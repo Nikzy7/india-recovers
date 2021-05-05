03715dd4b3ed2499c522e95e0bf54ecdb26b3660
Mukund Shukla <mukundshukla000@gmail.com>
2021-05-06 02:11:38 +0530

    CSS improvement

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var dataGet = () => {
     return new Promise(resolve => {
         url = 'https://api.covid19india.org/data.json'
@@ -18,9 +19,30 @@ function generateList(stateName,deltaRecovered){
         li.classList.add("list-group-item");
         li.appendChild(name);
         ul.appendChild(li);
+        
     }
 }
+function sortList(ul){
+    var new_ul = ul.cloneNode(false);
 
+    // Add all lis to an array
+    var lis = [];
+    for(var i = ul.childNodes.length; i--;){
+        if(ul.childNodes[i].nodeName === 'LI')
+            lis.push(ul.childNodes[i]);
+    }
+
+    // Sort the lis in descending order
+    lis.sort(function(a, b){
+       return parseInt(b.childNodes[0].data , 10) - 
+              parseInt(a.childNodes[0].data , 10);
+    });
+
+    // Add them into the ul in order
+    for(var i = 0; i < lis.length; i++)
+        new_ul.appendChild(lis[i]);
+    ul.parentNode.replaceChild(new_ul, ul);
+}
 var dynamicHead = (deltaRecovered) => {
 
     if(deltaRecovered==0){
